406cc2c70358927fea6d51f1da67e5cd00e1e7f2
Mykola Kuzyk <ukrboy99@gmail.com>
2021-03-31 23:12:25 +0200

    update Casting fields

--- Net_Conversions_Example/_conversion.cs
+++ Net_Conversions_Example/_conversion.cs
@@ -8,8 +8,42 @@ namespace Net_Conversions_Example
     {
         static void StartConverting()
         {
-            Convert.ToByte("10111", 2);
-            Convert.ToByte("11111111",2);
+            /* Byte */
+            Convert.ToByte("10111", 2); // returns 23 - the first argument 10111 is the nuber to convert, second is its base 
+            Convert.ToByte("11111111",2); // returns 255
+
+            /* Short */
+            Convert.ToInt16("111111111111111", 2); // returns 32767
+
+            /* Int */
+            Convert.ToInt32("11111111111111111111111111111"); //return 2147483647
+
+        }
+
+
+        static void CastingADecimal()
+        {
+            float myFloatValue = 10;
+            int myIntValue = (int)myFloatValue;
+            Console.WriteLine("myIntValue is " + myIntValue);
+
+            int NewInt = 10;
+            byte NewByte = (byte)NewInt;
+            double myDouble = (double)NewByte;
+            string myString = "false";
+            myString = NewInt.ToString();
+            short myShort = (short)NewInt;
+            char myChar = 'x';
+            long myLong = (long)NewInt;
+            decimal myDecimal = (decimal)myLong;
+            myString =  myString + 
+                        NewInt + 
+                        NewByte +
+                        myDouble +
+                        myChar;
+
+
+
         }
     }
 }

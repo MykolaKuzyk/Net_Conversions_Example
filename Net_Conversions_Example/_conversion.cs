4ac8a6fd1b28a1576cf8c7c715a44c3f092caf1d
Mykola Kuzyk <ukrboy99@gmail.com>
2021-03-31 22:17:47 +0200

    Add project files.

---
+++ Net_Conversions_Example/_conversion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net_Conversions_Example
+{
+    class _conversion
+    {
+        static void StartConverting()
+        {
+            Convert.ToByte("10111", 2);
+            Convert.ToByte("11111111",2);
+        }
+    }
+}
